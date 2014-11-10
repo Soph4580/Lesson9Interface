6cf93bbfd7ff5c0823d14a44fa94703aad857b18
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-11-10 09:04:25 -0500

    pt2

--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,58 @@
+
+import TurtleGraphics.Pen;
+
 public class Circle implements Shape {
 
+     private double xpos, ypos, radius;
+   
+    public Circle(){
+    xpos=0;
+    ypos=0;
+    radius=1;
+    }
+    
+    public Circle(double x, double y, double r)
+    {
+        xpos=x;
+        ypos=y;
+        radius=r;
+    }
+     
+    
      public double area() {
-        return 0;
+        return Math.PI*radius*radius;
+    }
+
+    
+    public void draw(Pen p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public double xpos() {
+        return xpos;
+    }
+
+    
+    public double ypos() {
+      return ypos;
+    }
+
+    
+    public void wave(double xloc, double yloc) {
+          xpos= xloc;
+            ypos= yloc;
+    }
+
+    public void stretchyBy(double factor) {
+       radius*=factor;
+    }
+    public String toString(){
+        String str = "This is a CIRCLE\n";
+        str+="XLOC: " + xpos + "\tYLOC: " + ypos + "\n";
+        str+="Radius: " + radius + "\n";
+        str+="Area: " + area();
+        return str;
     }
     
 }
--- src/Rect.java
+++ src/Rect.java
@@ -1,7 +1,61 @@
-public class Rect implements Shape {
 
+import TurtleGraphics.Pen;
+
+public class Rect implements Shape {
+    
+    private double xpos, ypos, width, length;
+    
+    public Rect(){
+    xpos=0;
+    ypos=0;
+    width=1;
+    length=1;
+    }
+    
+    public Rect(double x, double y, double w, double l)
+    {
+        xpos= x;
+        ypos= y;
+        width= w;
+        length= l;
+    }
+    
     public double area() {
-        return 0;
+        return width*length;
+    }
+
+   
+    public void draw(Pen p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+    public double xpos() {
+       return xpos;
+    }
+
+  
+    public double ypos() {
+        return ypos;
+    }
+
+    
+    public void wave(double xloc, double yloc) {
+         xpos= xloc;
+         ypos= yloc;
+    }
+
+    
+    public void stretchyBy(double factor) {
+       length*=factor;
+       width*=factor;
+    }
+    public String toString(){
+        String str = "This is a RECTANGLE\n";
+        str+="XLOC: " + xpos + "\tYLOC: " + ypos + "\n";
+        str+="Width: " + width + "\nHeight: " + length + "\n";
+        str+="Area: " + area();
+        return str;
     }
     
 }
