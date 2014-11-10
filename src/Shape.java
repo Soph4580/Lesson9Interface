6e163055bc168b04369d287de43e1169fef94683
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-11-10 08:41:22 -0500

    pt1

--- src/Shape.java
+++ src/Shape.java
@@ -2,5 +2,12 @@ import TurtleGraphics.*;
 
 public interface Shape {
     public double area();
+    public void draw(Pen p);
+    public double xpos();
+    public double ypos();
+    public void wave(double xloc, double yloc);
+    public void stretchyBy(double factor);
+    public String toString();
+    
     
 }
